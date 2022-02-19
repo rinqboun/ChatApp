9b674277a04319ed72df55fed68c61bf1c19a12f
rinqboun <adam.b.checinski@gmail.com>
2022-02-19 18:56:30 +0100

    create profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
 ]
 
 @NgModule({
