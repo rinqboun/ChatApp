5b75de01ca565d4e6c60cc480196ebd8c1489e12
rinqboun <adam.b.checinski@gmail.com>
2022-02-19 17:29:26 +0100

    implement login and register in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import {HttpHeaders, HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+const AUTH_API = 'http://localhost:8080/api/auth/'
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        email,
+        password
+      },
+      httpOptions
+    )
+  }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        email,
+        password
+      },
+      httpOptions
+    )
+  }
 }
