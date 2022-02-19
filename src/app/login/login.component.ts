acbbdd6950a6acb272b729fe6061dcc8db317a74
rinqboun <adam.b.checinski@gmail.com>
2022-02-19 18:45:12 +0100

    create login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {TokenStorageService} from "../services/token-storage.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    email: null,
+    password: null
+  }
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = ''
+  roles : string[] = []
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) { }
 
   ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true
+      this.roles = this.tokenStorage.getUser().roles
+    }
+  }
+
+  onSubmit(): void {
+    const {email, password} = this.form
+
+    this.authService.login(email, password)
+      .subscribe({
+        next: data => {
+          this.tokenStorage.saveToken(data.accessToken)
+          this.tokenStorage.saveUser(data)
+          this.isLoginFailed = false
+          this.isLoggedIn = true
+          this.roles = this.tokenStorage.getUser().roles
+          this.reloadPage()
+        },
+        error: err => {
+          this.errorMessage = err.error.message
+          this.isLoginFailed = true
+        }
+      })
+  }
+
+  reloadPage(): void {
+    window.location.reload()
   }
 
 }
