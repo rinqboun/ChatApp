2dec73ad72b8c7f844aa162b0de587d16fc668c8
rinqboun <adam.b.checinski@gmail.com>
2022-02-19 17:22:30 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+//
+// import { authInterceptor } from './helpers/auth.interceptor';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+]
 
 @NgModule({
   declarations: [
@@ -16,7 +27,8 @@ import { ProfileComponent } from './profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
